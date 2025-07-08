164f36873353f0649c3d41ac2b56a49b7f996228
MartinMBorn <Martin.M.Born@deutschebahn.com>
2025-07-08 15:39:39 +0200

    move index.html to root level

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,20 @@
-<template>
-  <h1>Hello ....</h1>
-</template>
-
-<script setup>
-console.log('👋 This message is being logged by "App.vue", included via Vite');
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World",
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
 </script>
+
+<template>
+  <h1>{{message}}</h1>
+  <button @click="increment">{{ count }}</button>
+</template>
