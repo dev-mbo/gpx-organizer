1c5516d7ce5e760d26ced1ff1caf4ae10a153d0e
dev-mbo <mbo123@posteo.de>
2025-07-13 19:04:36 +0200

    Add Openlayers Map

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,10 +1,14 @@
 <script>
+import Map from './components/Map.vue'
 export default {
   data() {
     return {
       message: "Hello World",
       count: 0
     }
+  },
+  components: {
+    Map
   },
   methods: {
     increment() {
@@ -17,4 +21,18 @@ export default {
 <template>
   <h1>{{message}}</h1>
   <button @click="increment">{{ count }}</button>
+  <Map />
 </template>
+
+<style>
+h1 {
+  color: red;
+}
+html, body {
+  height: 100%;
+  margin: 0;
+}
+#app {
+  height: 100%
+}
+</style>
---
+++ src/renderer/components/Map.vue
@@ -0,0 +1,31 @@
+<script>
+import {Map, View, Overlay} from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+import 'ol/ol.css'
+
+export default {
+    mounted() {
+        new Map({
+            target: this.$refs['map'],
+            layers: [
+                // adding a background tiled layer
+                new TileLayer({
+                    source: new OSM() // tiles are served by OpenStreetMap
+                }),
+            ],
+
+            // the map view will initially show the whole world
+            view: new View({
+                zoom: 0,
+                center: [0, 0],
+                constrainResolution: true
+            }),
+        })
+    }
+}
+</script>
+
+<template>
+<div ref="map" style="width:100%; height: 100%;"></div>
+</template>
