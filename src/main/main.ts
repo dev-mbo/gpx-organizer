a7460f5f0706229a10f8310edcc8058e64ceb2ce
dev-mbo <mbo123@posteo.de>
2026-02-22 21:18:54 +0100

    more work ...

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,8 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
+const gpxParse = require('gpx-parse');
+const fs = require('node:fs');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -10,8 +12,8 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -31,7 +33,41 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  /*ipcMain.handle('getGpx', (file) => { 
+    const gpxDir = 'src/public/gpx';
+    gpxParse.parseGpxFromFilepath.join(gpxDir, file), (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data.tracks[0].name)
+      }  
+    });
+  });*/
+  const getGpxFilesFromFolder = (folder = '') => {
+    const files = fs.readdirSync(path.join('src/public', folder), { withFileTypes: true });
+    let result = {};
+    result[folder] = [];
+    for (const file of files) {
+      if (file.isDirectory()) {
+        result = {
+          ...result, 
+          ...getGpxFilesFromFolder(path.join(folder, file.name))
+        };
+      } else {
+        result[folder].push(file.name);
+      }
+    }
+    return result;
+  };
+  ipcMain.handle('getFiles', () => {
+    const files = getGpxFilesFromFolder('gpx');
+    //const tracks = fs.readdirSync(gpxDir);
+    console.log(files);
+    return files;
+  });
+  createWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
--- src/renderer/components/Map.vue
+++ src/renderer/components/Map.vue
@@ -1,31 +1,86 @@
 <script>
-import {Map, View, Overlay} from 'ol'
+import {Map, View } from 'ol'
 import TileLayer from 'ol/layer/Tile'
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
 import OSM from 'ol/source/OSM'
+import GPX from 'ol/format/GPX'
+import Stroke from 'ol/style/Stroke.js';
+import Style from 'ol/style/Style.js';
+import { transform } from 'ol/proj'
 import 'ol/ol.css'
 
 export default {
-    mounted() {
-        new Map({
-            target: this.$refs['map'],
-            layers: [
-                // adding a background tiled layer
-                new TileLayer({
-                    source: new OSM() // tiles are served by OpenStreetMap
-                }),
-            ],
-
-            // the map view will initially show the whole world
-            view: new View({
-                zoom: 0,
-                center: [0, 0],
-                constrainResolution: true
+    data() {
+      return {
+        files: {},
+        selectedFile: null,
+        map: null,
+        gpxLayer: null,
+        style: {
+          'MultiLineString': new Style({
+            stroke: new Stroke({
+              color: 'navy',
+              width: 4,
             }),
-        })
+          }),
+        }
+      }
+    },
+    methods: {
+      async getFiles() {
+	      this.files = await gpx.getFiles();
+      },
+      async selectFile(dir, file) {
+        this.selectedFile = dir + '/' + file;
+        console.log(`select ${this.selectedFile}`);
+        if (this.gpxLayer) {
+          this.map.removeLayer(this.gpxLayer);
+        }
+        this.gpxLayer = new VectorLayer({
+          source: new VectorSource({
+            url: `${this.selectedFile}`,
+            format: new GPX()
+          }),
+          style: (feature) => this.style[feature.getGeometry().getType()]                   
+        });
+        this.map.addLayer(this.gpxLayer);
+      },
+    },
+    async mounted() {
+      await this.getFiles();
+      this.map = new Map({
+        target: this.$refs['map'],
+        layers: [
+          new TileLayer({
+            source: new OSM()
+          }),
+        ],
+        view: new View({
+          zoom: 11,
+          center: transform([8.2, 50], 'EPSG:4326', 'EPSG:3857'),
+          constrainResolution: true
+        }),
+      });
+      if (Object.keys(this.files).length) {
+        const dir = Object.keys(this.files)[0];
+        this.selectFile(this.files[dir][0]); 
+      }
     }
 }
 </script>
 
 <template>
+<ul v-if="files">
+  <li v-for="(files, dir) in files">
+    {{ dir }}
+    <ul v-if="files.length">
+      <li v-for="file of files">
+        <a href="#" @click="selectFile(dir, file)">{{ file }}</a>
+      </li>
+    </ul>
+  </li>
+</ul>
+<p v-else>no gpx tracks found in src/public/gpx</p>
 <div ref="map" style="width:100%; height: 100%;"></div>
 </template>
